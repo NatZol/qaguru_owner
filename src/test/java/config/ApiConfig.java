fa0ad0c3c7bf835f5b53878d583d4fab9e6703cb
NatZol <natalia.zlkh@gmail.com>
2021-10-02 19:09:17 +0300

    config and test

---
+++ src/test/java/config/ApiConfig.java
@@ -0,0 +1,18 @@
+package config;
+
+import org.aeonbits.owner.Config;
+
+@Config.LoadPolicy(Config.LoadType.MERGE)
+@Config.Sources({
+        "file:~/tmp/api.properties",
+        "classpath:api.properties",
+        "system:properties"
+})
+public interface ApiConfig extends Config {
+
+    @Key("baseUrl")
+    String baseUrl();
+
+    @Key("token")
+    String token();
+}
---
+++ src/test/java/config/WebConfig.java
@@ -0,0 +1,18 @@
+package config;
+
+import org.aeonbits.owner.Config;
+
+@Config.Sources({
+        "classpath:${remote}.properties"
+})
+public interface WebConfig extends Config {
+
+    @Key("browser")
+    String browser();
+
+    @Key("browserVersion")
+    String browserVersion();
+
+    @Key("isRemote")
+    Boolean isRemote();
+}
